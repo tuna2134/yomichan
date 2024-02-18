4ec7987ba74d3cfdae91311e9d51ab774040249e
コマリン親衛隊 <masato190411@icloud.com>
2024-02-18 00:52:03 +0000

    Update songbird input in events.rs

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,5 @@
 use crate::{tts::tts, StateRef};
-use songbird::input::cached::Memory;
+use songbird::{input::cached::Compressed, driver::Bitrate};
 use std::sync::Arc;
 use twilight_gateway::Event;
 
@@ -15,7 +15,7 @@ pub async fn handle_event(state: Arc<StateRef>, event: Event) -> anyhow::Result<
             if state.channel_ids.lock().await.contains(&message.channel_id) {
                 let audio = tts(message.content.clone(), 1).await?;
                 if let Some(manager) = state.songbird.get(message.guild_id.unwrap()) {
-                    let source = Memory::new(audio.into()).await?;
+                    let source = Compressed::new(audio.into(), Bitrate::Auto).await?;
                     let _ = source.raw.spawn_loader();
                     let mut handler = manager.lock().await;
                     handler.play_input(source.into());
